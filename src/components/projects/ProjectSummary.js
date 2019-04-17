513232717b34046b5db96bfcbaef3e23773db423
Tom Rem <rembisz.tomasz@gmail.com>
2019-04-17 11:36:30 +0200

    added two reducers and combine them plus added background and some styles

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProjectSummary = () => {
     return (
         <>
-            <div className="card z-depth-0 project-summary">
+            <div className="card z-depth-0 project-summary radius">
                 <div className="card-content grey-test test-darken-3">
                     <span className="card-title">Project Title</span>
                     <p>Posted by Tomuś</p>
