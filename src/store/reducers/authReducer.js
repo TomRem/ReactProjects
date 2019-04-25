19f5790d613a013f26418fbf512dd0830137a7cb
Tom Rem <rembisz.tomasz@gmail.com>
2019-04-25 13:47:21 +0200

    added firebase communication

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -15,7 +15,21 @@ const authReducer = (state = initState, action) => {
                 authError: null
             }
         case 'SIGNOUT_SUCCESS':
-            return state;
+            {
+                return state;
+            }
+        case 'SIGNUP_SUCCESS':
+            console.log('signup success');
+            return {
+                ...state,
+                authError: null
+            }
+        case 'SIGNUP_ERROR':
+            console.log('signup error');
+            return {
+                ...state,
+                authError: action.error.message
+            }
         default:
             return state;
     }
